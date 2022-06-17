4fe32a82a5b78a5333a1aa0a005b8e7e8acefd68
jrekoske <john.rekoske@gmail.com>
2022-06-16 22:08:17 -0700

    Updating install.

--- install.sh
+++ install.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
 VENV=romshake
-package_list=(
-      "cartopy"
-      "gmsh"
-      "h5py"
-      "joblib"
-      "matplotlib"
-      "netcdf4"
-      "numpy"
-      "openquake.engine"
-      "pandas"
-      "paramiko"
-      "pyproj"
-      "pyyaml"
-      "rasterio"
-      "scikit_learn"
-      "scipy"
-      "setuptools"
-      "tqdm"
-      "trimesh"
-)
-eval "$(micromamba shell hook --shell=bash)"
-micromamba activate base
-micromamba remove -y --name $VENV --all
-micromamba create -y -vvv --name $VENV ${package_list[*]}
-micromamba activate $VENV
-pip install seissolxdmf
-pip install seissolxdmfwriter
+eval "$(conda shell.bash hook)"
+conda activate base
+conda remove -y --name $VENV --all
+mamba create -n $VENV -y --file conda_requirements.txt -c conda-forge -vvv
+conda activate $VENV
+pip install -r pip_requirements.txt
 pip install --no-deps -e .
