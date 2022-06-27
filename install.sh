380814e0dacce47585d984b4351e062ded900f1e
jrekoske <john.rekoske@gmail.com>
2022-06-27 14:44:04 -0700

    Updating install.

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ conda remove -y --name $VENV --all
 mamba create -n $VENV -y --file conda_requirements.txt -c conda-forge -vvv
 conda activate $VENV
 pip install -r pip_requirements.txt
+mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 pip install --no-deps -e .
