28fdbbd812b634e273d68bc03023b1b3ef128ad5
jrekoske <john.rekoske@gmail.com>
2022-08-17 22:20:46 -0700

    Cleaning up.

--- scripts/run_cvhm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-INSTALL_DIR=/Users/jrekoske/my-programs/cvmh-15.1.0
-${INSTALL_DIR}/bin/vx_lite -s -z elev -m ${INSTALL_DIR}/model < infile-inner > outfile-inner
-${INSTALL_DIR}/bin/vx_lite -s -z elev -m ${INSTALL_DIR}/model < infile-outer > outfile-outer
